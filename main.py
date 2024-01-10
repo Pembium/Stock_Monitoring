064dc0782eb31dadb2ff9565e1165872476c5d7b
jacob <jacobpemberton022@gmail.com>
2024-01-10 17:16:58 +1300

    updating to print all data and plots

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from flask import Flask, render_template
 import json
 import sys
-
+import os
 app = Flask(__name__)
 
-@app.route("/")
+def read_json(file_path):
+	with open(file_path) as file:
+		data = json.load(file)
+	return data
+
+@app.route('/')
 
 def index():
-	with open('stock_summaries/test.json', 'r') as f:
-		data = json.load(f)
-	return render_template('index.html', data=data)
+	listings = read_json("listings.json").values()
+	stock_summaries = []
+	for listing in listings:
+		try:
+			stock = read_json(f'static/{listing}_summary.json')
+			stock_summaries.append(stock)
+			if os.path.isfile(f'static/6_month_data_{listing}.png'):
+
+				stock["IMG"] = f'6_month_data_{listing}.png'
+				stock["has_image"] = True
+			else:
+				stock["has_image"] = False
+		except:
+			print(f'Cannot find stock: {listing}')
+
+	return render_template('index.html', data=stock_summaries)
 
 if __name__ == '__main__':
 	debug = '--debug' in sys.argv
